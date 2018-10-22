f27db35a4ac83b2362d52275552169bf6fc42f95
paradoxfm <paradoxfm@mail.ru>
2018-10-22 22:35:42 +0500

    fix push track name

--- service/alarmService.go
+++ service/alarmService.go
@@ -13,10 +13,11 @@ const (
 )
 
 type AlarmConf struct {
-	UserId  string
-	PushIds []string
-	Zones   []model.GeoZoneDb
-	Pref    model.TrackPref
+	UserId    string
+	TrackName string
+	PushIds   []string
+	Zones     []model.GeoZoneDb
+	Pref      model.TrackPref
 }
 
 func createPushData(trackId string) (map[string]AlarmConf) {
@@ -25,6 +26,7 @@ func createPushData(trackId string) (map[string]AlarmConf) {
 	zones, _ := db.LoadZonesByTrackId(trackId)
 	for _, tr := range trackPref {
 		usrId := tr.UserId.String()
+		trName := tr.Name
 		zoneList := make([]model.GeoZoneDb, 0)
 		for _, zn := range zones {
 			if zn.UserId.String() == usrId {
@@ -33,7 +35,7 @@ func createPushData(trackId string) (map[string]AlarmConf) {
 		}
 		ids, _ := db.LoadUserPushIds(usrId)
 		if len(ids) != 0 {
-			rez[usrId] = AlarmConf{UserId: usrId, PushIds: ids, Pref: tr, Zones: zoneList}
+			rez[usrId] = AlarmConf{UserId: usrId, TrackName: trName, PushIds: ids, Pref: tr, Zones: zoneList}
 		}
 	}
 	return rez
@@ -50,6 +52,7 @@ func alarmsCheck(prev *model.Tracker, curr *model.Tracker, lowPowerAlarm bool, s
 			"tracker_name": curr.Id.String(),
 		}
 		for userId, conf := range confList {
+			data["tracker_name"] = conf.TrackName
 			SendPushForUser(userId, conf.PushIds, data)
 		}
 	}
@@ -87,7 +90,7 @@ func checkZonesForUser(pOld *geo.Point, pNew *geo.Point, conf AlarmConf) {
 			data := map[string]interface{}{
 				"message":      msgId,
 				"tracker_id":   conf.Pref.TrackId.String(),
-				"tracker_name": conf.Pref.Name,
+				"tracker_name": conf.TrackName,
 				"zone_name":    geoZone.Name,
 			}
 			SendPushForUser(conf.UserId, conf.PushIds, data)
